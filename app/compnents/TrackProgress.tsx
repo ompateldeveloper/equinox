0a4bd1aeb2dbf39f743cc3008772bd1ec961225a
ompateldeveloper <ompatel.developer@gmail.com>
2023-09-27 19:49:51 +0530

    type error fixed

--- app/compnents/TrackProgress.tsx
+++ app/compnents/TrackProgress.tsx
@@ -6,7 +6,7 @@ import Hint from "./Hint"
 
 export default function TrackProgress({handleRange,rangeRef,}:any) {
     const {setProgress,progress} = useGlobalContext()
-    const [randomArray,setRandomArray] = useState()
+    const [randomArray,setRandomArray] = useState<number[] | undefined> (undefined)
     useEffect(()=>{ 
         function generateRandomArray(){
             let randomArray= []
@@ -18,13 +18,13 @@ export default function TrackProgress({handleRange,rangeRef,}:any) {
             return randomArray
         }
         const ra = generateRandomArray()
-        // setRandomArray(ra)
+        setRandomArray(ra)
     },[])
 
     return (
         <div className='track-progress px-4 py-8 w-96 m-auto bg-zinc-700 flex items-center h-min justify-center rounded-lg relative overflow-hiden '>
             {rangeRef.current && <div className="track-progress-inner w-80 rounded overflow-hidden h-10 z-9 relative  ">
-                {/* <div className=" flex items-center h-full duration-100  peer/progress overflow-hidden absolute" style={{width:rangeRef?.current?.value/10+"%"}}>
+                <div className=" flex items-center h-full duration-100  peer/progress overflow-hidden absolute" style={{width:rangeRef?.current?.value/10+"%"}}>
                 {
                    randomArray?.map((val:any,index)=>{
                         return<div key={index} className="line shrink-0 self-center bg-zinc-500 rounded " style={{height:val+"px",width:"6px",marginLeft:2}}></div>
@@ -37,7 +37,7 @@ export default function TrackProgress({handleRange,rangeRef,}:any) {
                         return<div key={index} className="line bg-white rounded" style={{height:val+"px",width:"6px",marginLeft:2}}></div>
                     })
                 }
-                </div> */}
+                </div>
             </div>}
             
             {/* <Hint value={"Progress"}/> */}
