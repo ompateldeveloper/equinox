60272381db5c177ea5a73156d2ba0fb5eb4369f0
ompateldeveloper <ompatel.developer@gmail.com>
2023-11-10 21:05:32 +0530

    removed music causing problems

--- app/api/music/route.js
+++
@@ -1,9 +0,0 @@
-// import { NextResponse } from "next/server";
-import connectMongoDB from "@/lib/connectMongoDB"
-import { NextResponse } from "next/server"
-import Music from "@/models/music"
-export async function GET(req,res){
-    await connectMongoDB()
-    const music = await Music.find()
-    return NextResponse.json(music)
-}
